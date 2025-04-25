09db4f6ed83d824ec8c7d33714b3621364ecc33a
ai-365 <ai-365@outlook.com>
2025-04-25 11:58:23 +0800

    尝试docs：false

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,6 +41,8 @@ const config = {
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
+        docs: false,
+        /*
         docs: {
           routeBasePath: '/',  // 设置docs的根目录为项目根目录
           sidebarPath: './sidebars.js',
@@ -49,6 +51,7 @@ const config = {
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
+        */
         blog: false,  // 取消博客功能
         theme: {
           customCss: './src/css/custom.css',
