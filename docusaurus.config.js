980b565e309ce0cfe77d31a681cb5442585e93ff
ai-365 <ai-365@outlook.com>
2025-04-29 15:52:28 +0800

    foot new

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -96,7 +96,7 @@ const config = {
       footer: {
         style: 'dark',
         
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: 'Copyright © 2015-2025. Built with Docusaurus.'
       },
       prism: {
         theme: prismThemes.github,
