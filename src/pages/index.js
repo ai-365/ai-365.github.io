d0fa3a634dc270209f999590c4bbb1c23aa0c2e3
ai-365 <ai-365@outlook.com>
2025-04-29 18:03:34 +0800

    1

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="Web/JavaScript——index">
+            to="Web/JavaScript——TOC">
             JavaScript Guide
           </Link>
           <Link
